acc9f7033d034e631e9a0e780b85d221956909fa
kyletekser04ph <kivlynx42@gmail.com>
2024-12-12 17:24:42 +0800

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -46,7 +46,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
+			myPrefix: "𝗆𝗒 𝗽𝗿𝗲𝗳𝗶𝘅 𝗂𝗌: %2"
 		}
 	},
 
